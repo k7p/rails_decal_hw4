df3410763789eb30eb2d5a4d6dab7ea63699c75b
Kishan <kpanchal7@berkeley.edu>
2015-10-09 15:07:58 -0700

    complete

---
+++ app/controllers/dogs_controller.rb
@@ -0,0 +1,28 @@
+class DogsController < ApplicationController
+  def new
+    @dog = Dog.new
+  end
+
+  def index
+    @dogs = Dog.all
+  end
+
+  def show
+    @dog = Dog.find(params[:id])
+  end
+
+  def create
+    @dog = Dog.create(dog_params)
+    if @dog.save
+      redirect_to @dog
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def dog_params
+    params.require(:dog).permit(:name, :age)
+  end
+end
